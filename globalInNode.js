5a863318fdd688cc4cc4db83bc5095d81452241e
Atishay Kumar <atishay1950@gamil.com>
2025-03-07 01:48:11 +0530

    globalInNode and moduleInNode

---
+++ globalInNode.js
@@ -0,0 +1,14 @@
+// Globals in NodeJS
+
+// 1. __dirname -> It will give you the directory path of the current module
+console.log(__dirname)
+
+// 2. require -> It is a function, thi scan help you to import code from other files
+console.log(require)
+
+// 3. process -> It is an object, It gives you information about the current process
+console.log(process)
+
+
+
+
---
+++ moduleInNode.js
@@ -0,0 +1,10 @@
+// module => module is just a reusable piece of js code that is present in a file
+// modules => To improve reusability of your code nd enhance maintainability as we can segregate our functions, classes and variable
+//  in different tiles, and then whenever we need them we can ise them in other files.
+
+// this concept is called module pattern in node.js
+
+
+// nodejs gives us two ways to implement this export and import
+// 1. CommonJS moduling system (older version)
+// 2. ES6 module system (newer version)
